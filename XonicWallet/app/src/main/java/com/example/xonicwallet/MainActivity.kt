872c2e7c3e9a50750ecdb4802cbdaf7807f90cff
Phan Dang Quy <dangquyphan@gmail.com>
2019-09-15 16:03:37 +0700

    Update Wallet 15/09/2019

--- XonicWallet/app/src/main/java/com/example/xonicwallet/MainActivity.kt
+++ XonicWallet/app/src/main/java/com/example/xonicwallet/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.xonicwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        importwallet.setOnClickListener {
+            val intent:  Intent = Intent(this, ImportWallet::class.java)
+            startActivity(intent)
+        }
     }
 }

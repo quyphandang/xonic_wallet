3aa1b6700b52481c757882dcc0c92acdb4415d72
Phan Dang Quy <dangquyphan@gmail.com>
2019-09-17 15:31:41 +0700

    Update Wallet 07/17/2019

---
+++ WalletXonic/app/src/main/java/com/example/walletxonic/ImportWallet.java
@@ -0,0 +1,14 @@
+package com.example.walletxonic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImportWallet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_wallet);
+    }
+}
